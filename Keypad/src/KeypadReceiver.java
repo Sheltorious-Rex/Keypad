e6a0a4792e4dfeab8f95ee334541591a5acb070d
BBleck <bbleck@unm.edu>
2020-09-17 16:16:22 -0600

    Update KeypadReceiver.java
    Added comments to interface

--- Keypad/src/KeypadReceiver.java
+++ Keypad/src/KeypadReceiver.java
@@ -1,4 +1,8 @@
-
+/**
+*  An interface that encapsulates communication between the keypad
+*  and a keypad receiver.  GUI can write to this interface to
+*  communicate key presses.
+*/
 public interface KeypadReceiver {
     public void addKeyPress(char key);
 }
